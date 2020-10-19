6cbb39fc6ec40c6db3f2675f9b749b949caf7f21
Lorenzo Dall'Amico <46966964+lorenzodallamico@users.noreply.github.com>
2020-10-19 11:42:18 +0200

    Delete runtests.jl

--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-import Pkg, Test
-
-using Test, Documenter, CoDeBetHe
-@testset "CoDeBetHe" begin
-    ... # other tests & testsets
-    doctest(CoDeBetHe; manual = false)
-    ... # other tests & testsets
-end
